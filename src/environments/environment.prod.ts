520b08424e54d027ea6626d2aa3c6544d81d8e85
Ricardo Micale <ricardomicale@gmail.com>
2021-03-06 07:41:49 -0400

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCSRqwi1RVKkact3CevxDXnIJI2oB0GafM",
+    authDomain: "microproyecto-2-rm.firebaseapp.com",
+    projectId: "microproyecto-2-rm",
+    storageBucket: "microproyecto-2-rm.appspot.com",
+    messagingSenderId: "1022483473762",
+    appId: "1:1022483473762:web:9d448227eef5286ae0b83b",
+    measurementId: "G-RQDTNRQ3TH"
+  },
 };
